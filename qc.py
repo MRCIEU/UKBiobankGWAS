06a203650e06eb38594dc662223d1a9583163a32
Ben Elsworth <be15516@bristol.ac.uk>
2021-08-04 12:19:53 +0100

    argparse

--- qc.py
+++ qc.py
@@ -1,12 +1,54 @@
 import pandas as pd
+import os
+import argparse
 import sys 
 
-PIPELINE_DATA= sys.argv[1]
-USER = sys.argv[2]
+# Create the parser
+parser = argparse.ArgumentParser(description='QC the data')
 
-print(PIPELINE_DATA,USER)
+# Add the arguments
+parser.add_argument('-p',
+                       '--path',
+                       type=str,
+                       help='the path to the pipeline data directory')
+parser.add_argument('-u',
+                       '--user',
+                       type=str,
+                       help='the user name')
 
-job_file=f'{PIPELINE_DATA}/data/phenotypes/{USER}/input/jobs.csv'
+args = parser.parse_args()
 
-job_df = pd.read_csv(job_file)
-print(job_df.head())
+def check_args():
+    if args.path is None:
+        print("path (-p) required")
+        sys.exit()
+    if not os.path.isdir(args.path):
+        print(f'The path specified does not exist: {args.path}')
+        sys.exit()
+    if args.user is None:
+        print("User (-u) required")
+        sys.exit()
+
+def read_jobs():
+    input_path = args.path
+    user = args.user
+
+    job_file=f'{input_path}/data/phenotypes/{user}/input/jobs.csv'
+    print(job_file)
+
+    try:
+        job_df = pd.read_csv(job_file)
+        print(job_df.head())
+        
+        return job_df
+    except:
+        print(f'Can not read {job_file}')
+        sys.exit()
+
+#def read_pheno(job_df):
+    #
+
+
+if __name__ == "__main__":
+    check_args()
+    job_df = read_jobs()
