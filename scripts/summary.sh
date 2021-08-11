909cd78700c4dc435d125dfc420dd9eb2eccc992
Benjamin Elsworth <be15516@bc4login2.bc4.acrc.priv>
2021-08-11 12:48:14 +0100

    writing out summaries

--- scripts/summary.sh
+++ scripts/summary.sh
@@ -8,14 +8,28 @@ then
   export $(cat .env | sed 's/#.*//g' | xargs)
 fi
 
+# create some output summary files
+bolt_out=bolt_jobs.txt
+plink_out=plink_jobs.txt
+if [ -f $bolt_out ]
+then
+  rm $bolt_out
+fi
+if [ -f $plink_out ]
+then
+  rm $plink_out
+fi
+
 for user in $PIPELINE_DATA/data/phenotypes/*/; do
-  echo user: $user
   for o in ${user}output/*/; do
-    echo output directory: $o
+    #echo output directory: $o
     for j in ${o}*/; do 
-      echo gwas job: $j
-      find $j -name "*_imputed.txt.gz"
-      find $j -name "*.plink.gz"
+      #echo gwas job: $j
+      if [ -d $j ]
+      then
+        find $j -name "*_imputed.txt.gz" ! -name "chr*_out_imputed.txt.gz" -size +10M >> $bolt_out
+        find $j -name "*.plink.gz" -size +10M >> $plink_out
+      fi
     done
   done  
 done 
